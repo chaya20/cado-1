703f2df8cc06aaf98e5816b737e2c8f260aac59a
Jiwoo <wisdomwoo@sookmyung.ac.kr>
2021-08-02 16:36:40 +0900

    add TTS

--- app/src/main/java/com/example/cado/ColorRcmd.java
+++ app/src/main/java/com/example/cado/ColorRcmd.java
@@ -1,25 +1,32 @@
 package com.example.cado;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class ColorRcmd extends AppCompatActivity {
+public class ColorRcmd extends AppCompatActivity  implements TextToSpeech.OnInitListener {
 
     private ArrayList<RcmdData>arrayList;
     private RcmdAdapter mainAdapter;
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
 
+    TextView tvTitle;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,13 @@ public class ColorRcmd extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rcmd);
 
+        tts = new TextToSpeech(this, this);
+
         Intent intent = getIntent();
         String object = intent.getStringExtra("object");
 
-        TextView textView = findViewById(R.id.tvTitle);
-        textView.setText(object);
+        tvTitle = findViewById(R.id.tvTitle);
+        tvTitle.setText(object);
 
         recyclerView=(RecyclerView)findViewById(R.id.rv);
         linearLayoutManager = new LinearLayoutManager(this);
@@ -41,6 +50,14 @@ public class ColorRcmd extends AppCompatActivity {
 
         mainAdapter = new RcmdAdapter(arrayList);
         recyclerView.setAdapter(mainAdapter);
+
+        tvTitle.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                speakOut();
+            }
+        });
 
         Button Btn1 = (Button)findViewById(R.id.Btn1);
         Btn1.setOnClickListener(new View.OnClickListener() {
@@ -52,5 +69,36 @@ public class ColorRcmd extends AppCompatActivity {
             }
         });
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut() {
+        CharSequence text = tvTitle.getText();
+        tts.setPitch((float) 0.6);
+        tts.setSpeechRate((float) 0.8);
+        tts.speak(text,TextToSpeech.QUEUE_FLUSH,null,"id1");
+    }
+
+    @Override public void onDestroy() {
+        if (tts != null) {
+            tts.stop(); tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.KOREA);
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                //tvTitle.setEnabled(true);
+                speakOut();
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
     }
 }
--- app/src/main/java/com/example/cado/ColorSpec.java
+++ app/src/main/java/com/example/cado/ColorSpec.java
@@ -2,22 +2,29 @@ package com.example.cado;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
-public class ColorSpec extends AppCompatActivity {
+import java.util.Locale;
+
+public class ColorSpec extends AppCompatActivity implements TextToSpeech.OnInitListener {
     TextView tvName;
     ImageView ivChip;
     EditText etMemo;
     Button btSave, btTop, btBottom;
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +34,11 @@ public class ColorSpec extends AppCompatActivity {
 
         tvName = findViewById(R.id.tvName);
         ivChip = findViewById(R.id.ivChip);
-        etMemo = findViewById(R.id.etMemo);
-        btSave = findViewById(R.id.btSave);
+        //etMemo = findViewById(R.id.etMemo);
+        //btSave = findViewById(R.id.btSave);
         btTop = findViewById(R.id.btTop);
         btBottom = findViewById(R.id.btBottom);
+        tts = new TextToSpeech(this, this);
 
         Intent intent = getIntent();
 
@@ -41,7 +49,7 @@ public class ColorSpec extends AppCompatActivity {
         Glide.with(this)
                 .load(colorChip)
                 .into(ivChip);
-
+        /*
         SharedPreferences sharedPreferences = getSharedPreferences("file", 0);
         String value = sharedPreferences.getString("key","");
         etMemo.setText(value);
@@ -51,33 +59,73 @@ public class ColorSpec extends AppCompatActivity {
             public void onClick(View v) {
                 createMemo(v);
             }
+        });*/
+
+        tvName.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                speakOut();
+            }
         });
+
 
         btTop.setText(colorName + " 상의와 어울리는 하의");
         btBottom.setText(colorName + " 하의와 어울리는 상의");
 
     }
-
+    /*
     public void createMemo (View v){
         SharedPreferences  sharedPreferences = getSharedPreferences("file", 0);
         SharedPreferences.Editor editor  = sharedPreferences.edit();
         String value = etMemo.getText().toString();
         editor.putString("key", value);
         editor.commit();
+    }*/
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut() {
+        CharSequence text = tvName.getText();
+        tts.setPitch((float) 0.6);
+        tts.setSpeechRate((float) 0.8);
+        tts.speak(text,TextToSpeech.QUEUE_FLUSH,null,"id1");
+    }
+    @Override public void onDestroy() {
+        if (tts != null) {
+            tts.stop(); tts.shutdown();
+        }
+        super.onDestroy();
     }
 
+
     public void topGoToRcmd(View v){
-        String colorName = findViewById(R.id.tvName).toString();
+        //String colorName = findViewById(R.id.tvName).toString();
         Intent intent = new Intent(this, ColorRcmd.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.putExtra("object", btTop.getText());
         startActivity(intent);
     }
 
-    public void btmGoToRcmd(View v){
-        String colorName = findViewById(R.id.tvName).toString();
-        Intent intent = new Intent(this, ColorRcmd.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);;
+    public void btmGoToRcmd(View v) {
+        //String colorName = findViewById(R.id.tvName).toString();
+        Intent intent = new Intent(this, ColorRcmd.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.putExtra("object", btBottom.getText());
         startActivity(intent);
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.KOREA);
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                //tvName.setEnabled(true);
+                speakOut();
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
 }
